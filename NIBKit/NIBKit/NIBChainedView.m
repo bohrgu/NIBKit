1bc68dc2dc4cd405550904a29f6b4bd0ee2433fa
Guillaume Bohr <guillaume.bohr@gmail.com>
2014-02-01 22:03:49 +0100

    Started Xcode library

---
+++ NIBKit/NIBKit/NIBChainedView.m
@@ -0,0 +1,115 @@
+/*
+ NIBChainedView.m
+ 
+ Copyright 01/01/2014 Guillaume Bohr
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "NIBChainedView.h"
+
+@implementation NIBChainedView
+
+- (void)removeFromSuperview
+{
+    if (!removedFromSuperview)
+    {
+        // Bind previous and next views
+        self.nextView.previousView = self.previousView;
+        self.previousView.nextView = self.nextView;
+        
+        // Reframe chained views
+        if (self.nextView)
+        {
+            CGRect nextFrame = self.nextView.frame;
+            nextFrame.origin.y = self.frame.origin.y;
+            [self.nextView setFrame:nextFrame];
+        }
+        else
+        {
+            [self reframeSuperviewUsingFrame:self.previousView.frame];
+        }
+        
+        // Call super method
+        [super removeFromSuperview];
+        
+        // Update status
+        removedFromSuperview = YES;
+    }
+}
+
+- (void)willMoveToWindow:(UIWindow *)newWindow
+{
+    if (!alreadyAppeared)
+    {
+        alreadyAppeared = YES;
+        self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
+        [self reframeChainedViews];
+    }
+}
+
+- (void)reframeChainedViews
+{
+    if (self.previousView)
+    {
+        [self.previousView reframeChainedViews];
+    }
+    else
+    {
+        [self reframeNextView];
+    }
+}
+
+- (void)reframeNextView
+{
+    CGRect nextFrame = self.nextView.frame;
+    nextFrame.origin.y = CGRectGetMaxY(self.frame);
+    [self.nextView setFrame:nextFrame];
+}
+
+- (void)reframeSuperviewUsingFrame:(CGRect)rect
+{
+    if ([self.superview isMemberOfClass:[UIScrollView class]])
+    {
+        // Update content size
+        UIScrollView *superScrollView = (UIScrollView *)self.superview;
+        CGSize newContentSize = CGSizeMake(superScrollView.bounds.size.width, CGRectGetMaxY(rect));
+        [superScrollView setContentSize:newContentSize];
+    }
+    else
+    {
+        // Reframe super view
+        CGRect superFrame = self.superview.frame;
+        superFrame.size.height = CGRectGetMaxY(rect);
+        [self.superview setFrame:superFrame];
+    }
+}
+
+- (void)setFrame:(CGRect)frame
+{
+    [super setFrame:frame];
+    
+    if (alreadyAppeared)
+    {
+        if (self.nextView)
+        {
+            [self reframeNextView];
+        }
+        else
+        {
+            [self reframeSuperviewUsingFrame:self.frame];
+        }
+    }
+}
+
+@end
---
+++ NIBKit/NIBKit/NIBView.m
@@ -0,0 +1,114 @@
+/*
+ NIBView.m
+ 
+ Copyright 01/01/2014 Guillaume Bohr
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "NIBView.h"
+#import "NIBController.h"
+
+@implementation NIBView
+
+- (id)awakeAfterUsingCoder:(NSCoder *)aDecoder
+{
+    // Avoid init loop
+    BOOL isPlaceholder = ([[self subviews] count] == 0);
+    
+    if (isPlaceholder)
+    {
+        NIBView *nibView = [NIBView loadInstanceUsingPlaceholder:self];
+        return nibView;
+    }
+    
+    return self;
+}
+
++ (NIBView *)loadInstanceUsingPlaceholder:(NIBView *)placeholder
+{
+    NIBView *view = nil;
+    
+    // Get items in nib
+    UINib *nib = [UINib nibWithNibName:NSStringFromClass([placeholder class]) bundle:nil];
+    NSArray *itemsInNib = [nib instantiateWithOwner:self options:nil];
+    
+    // Look for object of current class
+    for (id item in itemsInNib)
+    {
+        if ([item isMemberOfClass:[placeholder class]])
+        {
+            // Set found view
+            view = item;
+            
+            // Pass placeholder content mode through
+            view.contentMode = placeholder.contentMode;
+            
+            // Pass placeholder tag through
+            view.tag = placeholder.tag;
+            
+            // Pass placeholder interaction properties through
+            view.userInteractionEnabled = placeholder.userInteractionEnabled;
+            view.multipleTouchEnabled = placeholder.multipleTouchEnabled;
+            
+            // Pass placeholder alpha through
+            view.alpha = placeholder.alpha;
+            
+            // Pass placeholder background through
+            view.backgroundColor = placeholder.backgroundColor;
+            
+            // Pass placeholder drawing properties through
+            view.opaque = placeholder.opaque;
+            view.hidden = placeholder.hidden;
+            view.clearsContextBeforeDrawing = placeholder.clearsContextBeforeDrawing;
+            view.clipsToBounds = placeholder.clipsToBounds;
+            view.autoresizesSubviews = placeholder.autoresizesSubviews;
+            
+            // Pass placeholder frame properties through
+            view.frame = placeholder.frame;
+            view.autoresizingMask = placeholder.autoresizingMask;
+        }
+    }
+    
+    // Look for a controller
+    for (id item in itemsInNib)
+    {
+        if ([item isKindOfClass:[UIViewController class]])
+        {
+            UIViewController *controller = (UIViewController *)item;
+            view.selfController = controller;
+        }
+    }
+    
+    return view;
+}
+
+- (void)setParentController:(NIBController *)controller
+{
+    for (UIView *subview in self.subviews)
+    {
+        if ([subview isKindOfClass:[NIBView class]])
+        {
+            NIBView *nibView = (NIBView *)subview;
+            [nibView setParentController:controller];
+        }
+    }
+    
+    if (self.selfController)
+    {
+        [controller addChildViewController:self.selfController];
+        self.selfController = nil;
+    }
+}
+
+@end

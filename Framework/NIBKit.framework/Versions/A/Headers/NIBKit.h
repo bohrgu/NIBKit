92311ea3d8833516f5d4717766ad2660f996fc08
Guillaume Bohr <guillaume.bohr@gmail.com>
2014-02-01 23:52:29 +0100

    Added Framework to repo

---
+++ Framework/NIBKit.framework/Versions/A/Headers/NIBKit.h
@@ -0,0 +1,16 @@
+//
+//  NIBKit.h
+//  NIBKit
+//
+//  Created by Toom on 01/02/14.
+//  Copyright (c) 2014 SPToom. All rights reserved.
+//
+
+#ifndef NIBKit_NIBKit_h
+#define NIBKit_NIBKit_h
+
+#import <NIBKit/NIBChainedView.h>
+#import <NIBKit/NIBController.h>
+#import <NIBKit/NIBView.h>
+
+#endif
